388ad1c4bbb7f03d8dc603208c2a60f9aacceaac
James Liu <james@jamesliu.io>
2021-05-13 15:59:08 +1000

    feat: add vector multiplication

--- src/vectors/core.clj
+++ src/vectors/core.clj
@@ -15,3 +15,9 @@
   (make
     (reduce - (map #(:x %) vectors))
     (reduce - (map #(:y %) vectors))))
+
+(defn mult
+  [vector n]
+  (make
+    (* (:x vector) n)
+    (* (:y vector) n)))
---
+++ src/vectors/ex_1_4.clj
@@ -0,0 +1,36 @@
+(ns vectors.ex-1-4
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [vectors.core :as vec]))
+
+(defn setup-state
+  []
+  {:mouse (vec/make (q/mouse-x)
+                    (q/mouse-y))
+   :centre (vec/make (/ (q/width) 2)
+                     (/ (q/height) 2))})
+
+(defn draw-state
+  [{:keys [mouse centre]}]
+  (q/background 255)
+
+  (q/translate (:x centre)
+               (:y centre))
+
+  (q/line 0 0 (:x mouse) (:y mouse)))
+
+(defn update-state
+  [{:keys [centre]}]
+  (let [mouse (vec/make (q/mouse-x)
+                        (q/mouse-y))]
+    {:mouse (vec/mult (vec/sub mouse centre) 0.5)
+     :centre centre}))
+
+(q/defsketch ex-1-4
+             :title "Vectors - Ex 1.4"
+             :settings #(q/smooth 2)
+             :setup setup-state
+             :draw draw-state
+             :update update-state
+             :size [640 360]
+             :middleware [m/fun-mode])
