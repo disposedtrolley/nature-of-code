ee0898f65d2ddb68582a70dd0cdfdc2885f98879
James Liu <james@jamesliu.io>
2021-05-17 22:06:23 +1000

    refactor: use assoc to update mover

--- src/vectors/mover.clj
+++ src/vectors/mover.clj
@@ -16,24 +16,23 @@
     :topspeed     topspeed}))
 
 (defn apply-force
-  [{:keys [location velocity acceleration mass topspeed]} force]
+  [{:keys [velocity acceleration mass] :as mover} force]
   (let [f (vec/div force mass)]
-    (make location (vec/add f velocity) (vec/add f acceleration) mass topspeed)))
+    (assoc mover
+      :velocity (vec/add f velocity)
+      :acceleration (vec/add f acceleration))))
 
 (defn tick
-  [{:keys [location velocity acceleration mass topspeed]}]
-  (let [new-velocity
-        (vec/limit (vec/add velocity acceleration) topspeed)]
-    (make
-      (vec/add new-velocity location)
-      new-velocity
-      (vec/mult acceleration 0)
-      mass
-      topspeed)))
+  [{:keys [location velocity acceleration topspeed] :as mover}]
+  (let [v (vec/limit (vec/add velocity acceleration) topspeed)]
+    (assoc mover
+      :location (vec/add v location)
+      :velocity v
+      :acceleration (vec/mult acceleration 0))))
 
 (defn check-edges
-  [{:keys [location velocity acceleration mass topspeed]} bounds-x bounds-y]
-  (let [new-location
+  [{:keys [location] :as mover} bounds-x bounds-y]
+  (let [l
         (vec/make
           (cond
             (> (location :x) bounds-x) 0
@@ -43,4 +42,5 @@
             (> (location :y) bounds-y) 0
             (< (location :y) 0) bounds-y
             :else (location :y)))]
-    (make new-location velocity acceleration mass topspeed)))
+    (assoc mover
+      :location l)))
